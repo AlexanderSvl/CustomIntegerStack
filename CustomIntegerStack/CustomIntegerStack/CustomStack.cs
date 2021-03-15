04444ab81e984cac771b45602a9c0e718a3819c8
AlexanderSvl <80323431+AlexanderSvl@users.noreply.github.com>
2021-03-15 23:08:59 +0200

    Add files via upload

---
+++ CustomIntegerStack/CustomIntegerStack/CustomStack.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CustomIntegerStack
+{
+    public class CustomStack<T>
+    {
+        private readonly IList<T> stackData;
+
+        public CustomStack()
+        {
+            this.stackData = new List<T>();
+        }
+
+        public void Push(params T[] items)
+        {
+            foreach (T item in items)
+            {
+                this.stackData.Add(item);
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine($"Element {item} added to stack.");
+                Console.ResetColor();
+            }
+        }
+
+        public void Pop()
+        {
+            if (this.stackData.Count == 0)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("No elements in the stack. Use command PUSH to add an item.");
+                Console.ResetColor();
+            }
+            else
+            {
+                T element = this.stackData[this.stackData.Count - 1];
+                int index = this.stackData.Count - 1;
+
+                this.stackData.RemoveAt(this.stackData.Count - 1);
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine($"Element {element} at index {index} is removed.");
+                Console.ResetColor();
+            }
+        }
+
+        public void Peek()
+        {
+            if (this.stackData.Count == 0)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("No elements in the stack. Use command PUSH to add an item.");
+                Console.ResetColor();
+            }
+            else
+            {
+                T element = this.stackData[this.stackData.Count - 1];
+                int index = this.stackData.Count - 1;
+
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine($"Peeked element {element} is at index {index}.");
+                Console.ResetColor();
+            }
+        }
+
+        public void Print()
+        {
+            if (stackData.Count != 0)
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.Write("Elements in stack: ");
+                Console.WriteLine(String.Join(", ", stackData));
+                Console.ResetColor();
+            }
+            else
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("No elements in the stack. Use command PUSH to add an item.");
+                Console.ResetColor();
+            }
+        }
+
+        public void Empty()
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("Stack is cleared.");
+            Console.ResetColor();
+
+            this.stackData.Clear();
+        }
+    }
+}
---
+++ CustomIntegerStack/CustomIntegerStack/Program.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Linq;
+
+namespace CustomIntegerStack
+{
+    public class StartUp
+    {
+        public static void Main()
+        {
+            CustomStack<int> customStack = new CustomStack<int>();
+
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine("Enter one of the following commands:");
+            Console.WriteLine("PUSH - Pushes an element/s into the stack.");
+            Console.WriteLine("POP - Pops an element from the stack.");
+            Console.WriteLine("PEEK - Peeks the element at the top of the stack.");
+            Console.WriteLine("CLEAR - Clears the stack.");
+            Console.WriteLine("END - Exit the program.");
+            Console.WriteLine("PRINT - Prints the stack into the console.");
+            Console.WriteLine();
+            Console.ResetColor();
+
+            string input = Console.ReadLine();
+
+            while (input != "END")
+            {
+                string[] parts = input.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+                string command = parts[0];
+
+                try
+                {
+                    switch (command)
+                    {
+                        case "PUSH":
+                            if (parts.Length == 1)
+                            {
+                                Console.ForegroundColor = ConsoleColor.Red;
+                                Console.WriteLine("Command PUSH requires element/s to push. Example: PUSH 3; PUSH 5 6; PUSH abc def ghi;");
+                                Console.ResetColor();
+                            }
+                            else
+                            {
+                                customStack.Push(parts.Skip(1).Select(int.Parse).ToArray());
+                            }
+
+                            break;
+                        case "POP":
+                            customStack.Pop();
+                            break;
+                        case "PEEK":
+                            customStack.Peek();
+                            break;
+                        case "PRINT":
+                            customStack.Print();
+                            break;
+                        case "CLEAR":
+                            customStack.Empty();
+                            break;
+                        case "END":
+                            break;
+                        default:
+                            Console.ForegroundColor = ConsoleColor.Red;
+                            Console.WriteLine("Command is not recognised");
+                            Console.ResetColor();
+                            break;
+                    }
+                }
+                catch (Exception exp)
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine(exp.Message);
+                    Console.ResetColor();
+                }
+
+                Console.ForegroundColor = ConsoleColor.Blue;
+                Console.WriteLine();
+                Console.WriteLine("Enter one of the following commands:");
+                Console.WriteLine("PUSH - Pushes an element into the stack.");
+                Console.WriteLine("POP - Pops an element from the stack.");
+                Console.WriteLine("END - Exit the program.");
+                Console.WriteLine("PRINT - Prints the stack into the console.");
+                Console.WriteLine();
+                Console.ResetColor();
+
+                input = Console.ReadLine();
+            }
+
+        }
+    }
+}
